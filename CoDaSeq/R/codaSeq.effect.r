89c7769efdf04c25c6a5c8537cfb6a9045179c41
ggloor <ggloor@uwo.ca>
2018-07-12 14:56:48 -0400

    begun roxygen conversion

--- CoDaSeq/R/codaSeq.effect.r
+++ CoDaSeq/R/codaSeq.effect.r
@@ -1,16 +1,20 @@
-#####################
-# return effect size and MMAD
-# requries a vector denoting group membership
-#####
+#' Estimate effect size between two distributions
+#'
+#' Calculates relatively robust measures of standardized mean difference (effect) and dispersion from a matrix or dataframe of numbers. The output value is smaller thant Cohen's d by a factor of 1.418 when comparing two Normal distributions. There is an option to normalize the effect size by that factor.
+#' @param x A numerical matrix with samples by column
+#' @param conds A vector denoting group membership
+#' @param corrected Whether to scale to Cohen's d or not, default is FALSE
+#' @return returns a vector of effect sizes
+#' @export
+#' @examples
+#' # make a synthetic dataset
+#' d <- c(rnorm(100,0,1), rnorm(100,2,1))
+#' e <- c(rnorm(100,2,1), rnorm(100,0,1))
+#'  de <- rbind(d,e)
+#' conds <- c(rep("A", 100), rep("B",100))
+#' # values should be approximately -2 and 2
+#' codaSeq.effect(de, conds, corrected=TRUE)
 
-# vectorized maximum absolute deviation
-codaSeq.mxad <- function(x,y){
-  pmax(abs(x - sample(x, size=10*length(x), replace=T)), abs(y - sample(y,size=10*length(y), replace=T)))
-}
-
-codaSeq.dnef <- function(a,b){
-   return(median( (a - b)/ (codaSeq.mxad(a,b)) ) )
-}
 
 codaSeq.effect <- function(x, conds,corrected=FALSE){
     conds <- as.factor(conds)
@@ -23,8 +27,18 @@ codaSeq.effect <- function(x, conds,corrected=FALSE){
     }
     return.data <- vector()
     for(i in 1:nrow(x)){
-        return.data[i] <- (codaSeq.dnef(as.numeric(x[i,levels[[1]] ]), as.numeric(x[i,levels[[2]] ])))
+        return.data[i] <- (private.dnef(as.numeric(x[i,levels[[1]] ]), as.numeric(x[i,levels[[2]] ])))
     }
     if(corrected == FALSE) return(return.data)
     if(corrected == TRUE) return(return.data*1.418)
 }
+
+# vectorized maximum absolute deviation
+private.mxad <- function(x,y){
+  pmax(abs(x - sample(x, size=10*length(x), replace=T)), abs(y - sample(y,size=10*length(y), replace=T)))
+}
+
+private.dnef <- function(a,b){
+   return(median( (a - b)/ (private.mxad(a,b)) ) )
+}
+
